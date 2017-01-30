253d329cbdfe89055fb26b332073ea43b92e6b12
harshbhardwaj29 <harshbhardwajcsit@gmail.com>
2017-01-30 20:23:41 +0530

    trying without javax library in android

--- app/src/main/java/com/harsh/techinnova/facedex/SaveInfo.java
+++ app/src/main/java/com/harsh/techinnova/facedex/SaveInfo.java
@@ -10,14 +10,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-
 import org.apache.http.entity.StringEntity;
 import org.json.JSONObject;
 
-import java.io.OutputStreamWriter;
+import java.io.DataOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 
 /**
  * Created by samsung on 23-Jan-17.
@@ -30,16 +29,6 @@ public class SaveInfo extends Activity implements View.OnClickListener {
     String appId = "4e1fa2ad";
     String appKey = "ebe99430089461bd18189b5613ac82cc";
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client2;
 
 
     @Override
@@ -75,33 +64,70 @@ public class SaveInfo extends Activity implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.savebutton:
 
+                Toast.makeText(getBaseContext(), "1", Toast.LENGTH_LONG).show();
                 try {
                     /************** For getting response from HTTP URL start ***************/
                     URL object = new URL("https://api.kairos.com/enroll");
                     JSONObject json=new JSONObject();
                     json.put("image",bitmap);
                     json.put("subject_id",bio);
-
+                    Toast.makeText(getBaseContext(), "2", Toast.LENGTH_LONG).show();
+                    //open connection
                     HttpURLConnection connection = (HttpURLConnection) object.openConnection();
-                    // int timeOut = connection.getReadTimeout();
+
+                    //set time fields
                     connection.setReadTimeout(60 * 1000);
                     connection.setConnectTimeout(60 * 1000);
-                    String authHeader = getAuthHeader(appId,appKey);
-                    connection.setRequestMethod("POST");
+                    //String authHeader = getAuthHeader(appId,appKey);
+
+                    //set request type
+                    connection.setRequestMethod("GET");
                     connection.setDoInput(true);
                     StringEntity params = new StringEntity(json.toString());
-                    connection.setRequestProperty("Authorization", authHeader);
-                    connection.addRequestProperty("Content-Type", "application/json");
-                    OutputStreamWriter streamWriter = new OutputStreamWriter(connection.getOutputStream());
-                    streamWriter.write(json.toString());
-                    streamWriter.flush();
-                    StringBuilder stringBuilder = new StringBuilder();
+                    //set header
+                    Toast.makeText(getBaseContext(), "3", Toast.LENGTH_LONG).show();
 
+
+                    //set header
+                    connection.setRequestProperty("Content-Type", "application/json");
+                    connection.setRequestProperty("app_id",appId);
+                    connection.setRequestProperty("app_key",appKey);
+
+
+
+                    Toast.makeText(getBaseContext(), "4", Toast.LENGTH_LONG).show();
+
+                    //`5.1 Use Jackson object mapper to convert Contnet object into JSON
+                   /*
+                    System.out.println("status: " + response.getStatus());
+                    System.out.println("headers: " + response.getHeaders());
+                    System.out.println("body:" + response.readEntity(String.class));
+*/
+                    // 5.2 Get connection output stream
+                    DataOutputStream streamWriter = new DataOutputStream(connection.getOutputStream());
+
+                   // streamWriter.write(json);
                     Toast.makeText(getBaseContext(), "You are Done", Toast.LENGTH_LONG).show();
-                    connection.connect();
+
+                    streamWriter.flush();
+
+                    // 5.5 close
+                    streamWriter.close();
+
+
                     Toast.makeText(getBaseContext(), "You are not Done", Toast.LENGTH_LONG).show();
                     int responseCode = connection.getResponseCode();
                     String responseMsg = connection.getResponseMessage();
+
+
+
+
+
+
+
+
+
+
 
                     if (responseCode == 200) {
                         Toast.makeText(getBaseContext(), "You are Done", Toast.LENGTH_LONG).show();
